56c69516a94d13cabd7ac83f54db556ca64c1b4e
Glitch (glitch-blank-website) <none>
2023-01-17 03:45:43 +0000

    ⛺️🏪 Checkpoint ./.glitch-assets:52088767/1133 ./index.html:52088767/2067 ./script.js:52088767/819

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-console.log("Hello, world!");
+console.log("MR BEAST");
+
+
+var beast = document.createElement("video");  
+beast.setAttribute("src", "https://cdn.glitch.me/788ed3c5-1869-42bb-9e05-11c8fba0acdf/MR_BEAST.mp4?v=1673926556697");  
+beast.setAttribute("id", "beast"); 
+beast.setAttribute("width", "650"); 
+beast.setAttribute("autoplay", "true");
+beast.setAttribute("autostart", "true"); 
+document.body.appendChild(beast);
