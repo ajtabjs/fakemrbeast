f43a57fab0e57bb24a8400352b8d50a280b297b2
Glitch (glitch-blank-website) <none>
2023-01-17 04:07:54 +0000

    📕⛰ Checkpoint ./beast.js:52088767/908 ./index.html:52088767/15

--- beast.js
+++ beast.js
@@ -1,3 +1,17 @@
+console.log("MR BEAST");
+console.log("To whoever sees this, this webpage is a joke. Made by AJTalksAboutStuff")
+
+const video = document.getElementsByTagName("body");
+
+function auto() {
+     document.querySelector('body').addEventListener('click', () => {
+        document.querySelector('video').play();
+    });
+  
+  document.getElementById("beast").click();
+}
+
+function beast(){
 var beast = document.createElement("video");  
 beast.setAttribute("src", "https://cdn.glitch.me/788ed3c5-1869-42bb-9e05-11c8fba0acdf/MR_BEAST.mp4?v=1673926556697");  
 beast.setAttribute("id", "beast"); 
@@ -6,3 +20,9 @@ beast.setAttribute("width", "650");
 beast.setAttribute("autoplay", "true");
 beast.setAttribute("autostart", "true"); 
 document.body.appendChild(beast);
+  
+};
+
+beast();
+
+window.onload = auto();
