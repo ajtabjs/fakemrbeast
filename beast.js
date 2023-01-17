4ddbb42b5f6d615eb7fbdd4799125f0a8527c1b2
Glitch (glitch-blank-website) <none>
2023-01-17 03:56:53 +0000

    🐃👷 Checkpoint ./script.js:52088767/445 ./beast.js:52088767/802 ./index.html:52088767/1327 ./.glitch-assets:52088767/348

--- beast.js
+++ beast.js
@@ -1,17 +1,8 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-console.log("MR BEAST");
-
-function beast(){
-
 var beast = document.createElement("video");  
 beast.setAttribute("src", "https://cdn.glitch.me/788ed3c5-1869-42bb-9e05-11c8fba0acdf/MR_BEAST.mp4?v=1673926556697");  
 beast.setAttribute("id", "beast"); 
+  beast.setAttribute("controls", "autoplay"); 
 beast.setAttribute("width", "650"); 
 beast.setAttribute("autoplay", "true");
 beast.setAttribute("autostart", "true"); 
 document.body.appendChild(beast);
-}
-
-window.onload = beast();
