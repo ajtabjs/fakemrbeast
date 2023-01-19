2bcfe4dfc1786e1eef033338825134c04de9b8ff
Glitch (glitch-blank-website) <none>
2023-01-19 00:03:12 +0000

    🌿👔 Checkpoint ./pong.js:52088767/219

--- pong.js
+++ pong.js
@@ -1,17 +1,20 @@
 function div() {
-var text = document.createElement("div");  
-text.setAttribute("id", "statusbox");
-   text.style.borderColor = "black";
-  text.style.borderColor = "black";
+var div = document.createElement("div");  
+div.setAttribute("id", "statusbox");
+   div.style.borderRadius = "10px";
+  div.style.borderColor = "black";
+   document.body.appendChild(div);
 }
+div();
 
 
 function pong(){
 var text = document.createElement("p");  
 text.setAttribute("contenteditable","true");  
-text.style.fontFamily("Verdana, Trebuchet MS, Arial, Helvetica, sans-serif");
-  text.style.postition("absolute");
+//text.style.fontFamily("Verdana, Trebuchet MS, Arial, Helvetica, sans-serif");
+  text.style.position("absolute");
   text.style.float("right");
 text.setAttribute("id", "status");
-$('#iframecontainer').append(text);
+$('#statusbox').append(text);
 }
+pong();
