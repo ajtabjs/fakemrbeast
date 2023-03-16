4e80b632968bd9e7956304c750b4e33eae010ef0
Glitch (glitch-blank-website) <none>
2023-03-16 01:57:51 +0000

    🍐🐲 Checkpoint ./index.html:52088767/61 ./auto.js:52088767/747

--- auto.js
+++ auto.js
@@ -8,3 +8,18 @@ function auto() {
 
 auto();
 
+
+var browserName = (function (agent) {
+        switch (true) {
+            case agent.indexOf("edge") > -1: return "MS Edge";
+            case agent.indexOf("edg/") > -1: return "Edge ( chromium based)";
+            case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
+            case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
+            case agent.indexOf("trident") > -1: return "MS IE";
+            case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
+            case agent.indexOf("safari") > -1: return "Safari";
+            default: return "other";
+        }
+    })(window.navigator.userAgent.toLowerCase());
+
+document.querySelector("h1").innerText="You are using "+ browserName +" browser";  
