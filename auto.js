302b1e21d83e3c2f5af6d97834aa8ab11da5d194
Glitch (glitch-blank-website) <none>
2023-03-16 02:08:59 +0000

    👬👶 Checkpoint ./auto.js:52088767/911

--- auto.js
+++ auto.js
@@ -8,18 +8,16 @@ function auto() {
 
 auto();
 
+var vid = document.getElementById("beast");
 
 var browserName = (function (agent) {
         switch (true) {
             case agent.indexOf("edge") > -1: return "MS Edge";
             case agent.indexOf("edg/") > -1: return "Edge ( chromium based)";
             case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
-            case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
-            case agent.indexOf("trident") > -1: return "MS IE";
-            case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
-            case agent.indexOf("safari") > -1: return "Safari";
+            case agent.indexOf("chrome") > -1 && !!window.chrome: return vid.setAttribute("autoplay", muted);
+
+            case agent.indexOf("firefox") > -1: return document.querySelector("video").play();
             default: return "other";
         }
     })(window.navigator.userAgent.toLowerCase());
-
-document.querySelector("h1").innerText="You are using "+ browserName +" browser";  
